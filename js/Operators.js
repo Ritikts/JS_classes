f25056ac97cf5ab86fd91de683387fab43c5d51e
ritikts <webofritik@gmail.com>
2023-05-22 21:42:26 +0530

    code

---
+++ js/Operators.js
@@ -0,0 +1,35 @@
+// const num = 2 + 5 + "8";
+// console.log(num)
+
+// let x = 10;
+// x -= -5;
+// console.log(x);
+
+// let a = 6;
+// b = 6;
+// let b = 6;
+// a = 6;
+// console.log(a, b);
+
+// let a;
+// console.log(a);
+
+// var a = 10;
+
+// {
+//   var a = 20;
+// }
+
+// console.log(a);
+
+// var a = 3;
+// var a;
+// console.log(a);
+
+// var a;
+// let a;
+// console.log(a);
+
+// console.log(a);
+// var a = 6; 
+// console.log(a);
---
+++ js/variables.js
@@ -0,0 +1,36 @@
+// var num = 1;
+// var num = 2;
+// document.write(num);
+
+// In case of var we can Redeclare and Reassign the variable
+
+// let num2 = 4;
+// num2 = 6;
+// document.write(num2);
+// In case of let we can only Reassign the variable
+
+// const fix = "JS Class";
+
+// In case of const we can't change Reassign or Redeclare the value
+
+// Block Scope
+// Global Scope
+
+// Block Scope -- The variable and it's output will only be work in the function
+// Global Scope -- The variable can also work outside the function
+// let value2 = 70;
+// const value = () => {
+//   console.log(value2);
+// };
+// console.log(value2);
+// value();
+
+
+// Hoisting
+
+// In case of var the variable can only read it's intialization value
+// In case of let the varibale can't read it's intialization value and it will through an error
+// name2 = "Elcvvccvcvvcvcse" 
+// let name2 = "something"
+
+// console.log(name2)
